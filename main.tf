f15057f7be399741bf84301090847958e59b6752
simran <khotsimran04@gmail.com>
2023-05-04 18:50:33 +0530

    added autoscaling group

--- main.tf
+++ main.tf
@@ -5,12 +5,11 @@ resource "aws_launch_template" "mylaunch" {
 }
 
 resource "aws_autoscaling_group" "myauto" {
-  availability_zones = ["us-east-1a","us-east-1b","us-east-1c"]
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
   desired_capacity   = 1
   max_size           = 1
   min_size           = 1
   health_check_type = "EC2"
-
   launch_template {
     id      = aws_launch_template.mylaunch.id
     version = "$Latest"
