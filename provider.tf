c04340cfb426c8d934eba614e326434bdd77eb72
simran <khotsimran04@gmail.com>
2023-05-04 18:24:41 +0530

    added autoscaling group

--- provider.tf
+++ provider.tf
@@ -10,7 +10,5 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4AE6PGTTD3USY6GQ"
-  secret_key = "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"
 }
 
