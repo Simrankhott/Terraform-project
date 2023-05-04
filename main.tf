540beccc5a152b39967ff2f943d975537734f8cd
simran <khotsimran04@gmail.com>
2023-05-04 20:01:26 +0530

    added s3 as a backend for tfstate

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_autoscaling_group" "myauto" {
   desired_capacity   = 1
   max_size           = 1
   min_size           = 1
-  health_check_type = "EC2"
+  health_check_type  = "EC2"
   launch_template {
     id      = aws_launch_template.mylaunch.id
     version = "$Latest"
